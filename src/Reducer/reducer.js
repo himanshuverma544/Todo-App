1a1586e4a2676ea68618197e22e7ea129a85dd0d
Himanshu Verma <himanshuverma544@gmail.com>
2023-02-22 00:21:15 +0530

    update: - returning the updated todo within the array - returning the loaded todos array

--- src/Reducer/reducer.js
+++ src/Reducer/reducer.js
@@ -1,11 +1,17 @@
-import { ADD_TODO, REMOVE_TODO } from "./action.types";
+import { LOAD_TODOS, ADD_TODO, UPDATE_TODO, REMOVE_TODO } from "./action.types";
 
  export const todoReducer = (state, action) => {
 
   switch (action.type) {
 
+    case LOAD_TODOS:
+      return action.payload;
+
     case ADD_TODO:
       return [...state, action.payload];
+
+    case UPDATE_TODO:
+      return state.map(todo => (todo.id === action.payload.id ? {...todo, todoString : action.payload.todoString } : todo));
 
     case REMOVE_TODO:
       return state.filter(todo => (todo.id !== action.payload));
