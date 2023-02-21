474c6137ded5ef91ade2770ebec6fb8334c01562
Himanshu Verma <himanshuverma544@gmail.com>
2023-02-22 00:19:30 +0530

    update: handling edit todo by sending data to dispatcher

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,51 +1,80 @@
-import { Form, InputGroup, Input, Button } from "reactstrap"
+import { Form, InputGroup, Input, Button } from "reactstrap";
 import { toast } from "react-toastify";
-import { useContext, useState } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { TodoContext } from "../Context/Context";
-import { ADD_TODO } from "../Reducer/action.types";
+import { ADD_TODO, UPDATE_TODO } from "../Reducer/action.types";
 import { v4 as getKey } from "uuid";
 
 
 const TodoForm = () => {
 
   const [todoString, setTodoString] = useState("");
-  const { dispatch } = useContext(TodoContext);
+  const { todoToEdit, setTodoToEdit, dispatch } = useContext(TodoContext);
+  const todoInputField = useRef(null);
 
-  function handleAddToDo(event) {
+  useEffect(() => {
+    
+    if (todoToEdit.status === "EDIT") {
+      setTodoString(todoToEdit.todoString);
+
+      if (todoInputField.current) {
+        todoInputField.current.focus();
+      }
+    }
+  }, [todoToEdit.status ,todoToEdit.todoString]);
+
+
+  function handleSubmit(event) {
 
     event.preventDefault();
 
     if (!todoString) {
-      return toast("Please enter some text", {type: "error"});
+      return toast("Please enter some text.", {type: "error"});
     }
 
     const todo = {
       todoString,
-      id: getKey()
+      id: todoToEdit.status !== "EDIT" ? getKey() : todoToEdit.id
     };
 
-    dispatch({
-      type: ADD_TODO,
-      payload: todo
-    });
+    switch (todoToEdit.status) {
+      case "EDIT" :
+        dispatch({
+          type: UPDATE_TODO,
+          payload: todo
+        });
+        setTodoToEdit({});
+        todoToEdit.listToUpdate.classList.remove("selected-list-background-color");
+        toast("Todo Updated.", {type: "success"})
+        break;
 
+      default:
+        dispatch({
+          type: ADD_TODO,
+          payload: todo
+        });
+        toast("Todo Added.", {type: "success"})
+    }
+    
     setTodoString("");
   }
 
   return (
-    <Form onSubmit={handleAddToDo}>
+    <Form onSubmit={handleSubmit}>
         <InputGroup>
           <Input
             type="text"
+            innerRef={todoInputField}
             name="todo"
             id="todo"
             placeholder="Enter your Todo here"
             value={todoString}
             onChange={event => setTodoString(event.target.value)}
+            autoFocus
           />
           <Button
             color="warning">
-            ADD
+            {todoToEdit.status !== "EDIT" ? "ADD" : "UPDATE"}
           </Button>
         </InputGroup>
     </Form>
