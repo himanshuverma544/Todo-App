c58d13a4aa0461f6a638169aba5be708eeec1157
Himanshu Verma <himanshuverma544@gmail.com>
2023-02-22 00:12:49 +0530

    update: adding function to handle edit of todo

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,29 +1,37 @@
 import { useContext } from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
 import { TodoContext } from "../Context/Context";
 import { REMOVE_TODO } from "../Reducer/action.types";
-import { FaCheckDouble } from "react-icons/fa";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { FaCheckDouble, FaPen } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 
 const Todos = () => {
 
-  const {todos, dispatch} = useContext(TodoContext);
+  const {todos, handleEditTodo, dispatch} = useContext(TodoContext);
 
   return (
     <ListGroup className="mt-5 mb-2 items">
       {todos.map(todo => (
-        <ListGroupItem key={todo.id}>
+        <ListGroupItem className="list-item" key={todo.id}>
           {todo.todoString}
           <span 
-            className="todo-completed-icon"
+            className="todo-icons"
             onClick={() => {
               dispatch({
                 type: REMOVE_TODO,
                 payload: todo.id
               });
+              toast("Todo Removed.", {type: "success"});
             }}
           >
             <FaCheckDouble/>
+          </span>
+          <span
+            className="todo-icons px-3"
+            onClick={event => {handleEditTodo(event, todo.id)}}
+          >
+            <FaPen/>
           </span>
         </ListGroupItem>
       ))}
