f35574a299b7363eb720158a6f6dcf44d874fcdf
Himanshu Verma <himanshuverma544@gmail.com>
2024-03-04 00:59:24 +0530

    upd: displaying the list in reverse order

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,9 +1,8 @@
-import { useContext, memo } from "react";
+import { useContext } from "react";
 import { TodoContext } from "../Context/Context";
 import { REMOVE_TODO } from "../Reducer/action.types";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import { FaCheckDouble, FaPen } from "react-icons/fa";
-import { toast } from "react-toastify";
 
 
 const Todos = () => {
@@ -11,32 +10,34 @@ const Todos = () => {
   const {todos, handleEditTodo, dispatch} = useContext(TodoContext);
 
   return (
-    <ListGroup className="mt-5 mb-2 items">
-      {todos.map(todo => (
-        <ListGroupItem className="list-item" key={todo.id}>
-          {todo.todoString}
-          <span 
-            className="todo-icons"
-            onClick={() => {
-              dispatch({
-                type: REMOVE_TODO,
-                payload: todo.id
-              });
-              toast("Todo Removed.", {type: "success"});
-            }}
-          >
-            <FaCheckDouble/>
-          </span>
-          <span
-            className="todo-icons px-3"
-            onClick={event => {handleEditTodo(event, todo.id)}}
-          >
-            <FaPen/>
-          </span>
-        </ListGroupItem>
-      ))}
+    <ListGroup className="items mt-2 mb-2 ">
+      {todos.map((_, index, arr) => {
+        const todo = arr.at(-index - 1);
+        return (
+          <ListGroupItem className="list-item" key={todo.id}>
+            {todo.todoString}
+            <span 
+              className="todo-icons"
+              onClick={() => {
+                dispatch({
+                  type: REMOVE_TODO,
+                  payload: todo.id
+                });
+              }}
+            >
+              <FaCheckDouble/>
+            </span>
+            <span
+              className="todo-icons px-3"
+              onClick={event => {handleEditTodo(event, todo.id)}}
+            >
+              <FaPen/>
+            </span>
+          </ListGroupItem>
+        )
+      })}
     </ListGroup>
   );
 };
 
-export default memo(Todos);
+export default Todos;
